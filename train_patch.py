50077f6496d513f4fef312c00790601a2007a83f
nglod33 <nglod33@gmail.com>
2019-06-04 15:40:06 -0400

    Documentation added for train_patch.py. Still needs some work

--- train_patch.py
+++ train_patch.py
@@ -58,50 +58,75 @@ class PatchTrainer(object):
 
         # Generate stating point
         adv_patch_cpu = self.generate_patch("gray")
-        #adv_patch_cpu = self.read_image("saved_patches/patchnew0.jpg")
+        # adv_patch_cpu = self.read_image("saved_patches/patchnew0.jpg")
 
+        # Sets the patch tensor to have a gradient always, allowing the backward function
         adv_patch_cpu.requires_grad_(True)
 
+        # Sets up training and determines how long the training length will be
         train_loader = torch.utils.data.DataLoader(InriaDataset(self.config.img_dir, self.config.lab_dir, max_lab, img_size,
                                                                 shuffle=True),
                                                    batch_size=batch_size,
                                                    shuffle=True,
                                                    num_workers=10)
+
+        # Sets the epoch length
         self.epoch_length = len(train_loader)
         print(f'One epoch is {len(train_loader)}')
 
+        # Creates the object which will optimize the patch, and sets the learning rate
         optimizer = optim.Adam([adv_patch_cpu], lr=self.config.start_learning_rate, amsgrad=True)
+
+        # TODO: Find out exactly what this does. I assume it schedules tasks on the GPU
         scheduler = self.config.scheduler_factory(optimizer)
 
         et0 = time.time()
         for epoch in range(n_epochs):
+
+            # Sets the gradient inputs to zero
             ep_det_loss = 0
             ep_nps_loss = 0
             ep_tv_loss = 0
             ep_loss = 0
             bt0 = time.time()
+
+            # I have no fucking clue how long this is supposed to be running, probably for the epoch length? Needs
+            # More research
             for i_batch, (img_batch, lab_batch) in tqdm(enumerate(train_loader), desc=f'Running epoch {epoch}',
                                                         total=self.epoch_length):
                 with autograd.detect_anomaly():
+
+                    # Optimizes everything to run on GPUs
                     img_batch = img_batch.cuda()
                     lab_batch = lab_batch.cuda()
-                    #print('TRAINING EPOCH %i, BATCH %i'%(epoch, i_batch))
+                    # print('TRAINING EPOCH %i, BATCH %i'%(epoch, i_batch))
                     adv_patch = adv_patch_cpu.cuda()
+
+                    # Creates a patch transformer with a default grey patch. Can't find this object anywhere but most
+                    # Likely it allows the patch to be moved around on inputted images.
+                    # TODO: Find documentation for this object
                     adv_batch_t = self.patch_transformer(adv_patch, lab_batch, img_size, do_rotate=True, rand_loc=False)
+
+                    # Can't find this object anywhere else, most likely allows the patch to be put into inputted photos
+                    # TODO: find documentation for this object
                     p_img_batch = self.patch_applier(img_batch, adv_batch_t)
                     p_img_batch = F.interpolate(p_img_batch, (self.darknet_model.height, self.darknet_model.width))
 
+
+                    # TODO: Figure out exactly what these transforms are doing
                     img = p_img_batch[1, :, :,]
                     img = transforms.ToPILImage()(img.detach().cpu())
-                    #img.show()
+                    # img.show()
 
-
+                    # Looks to be where the given patch is evaluated, however all these objects are not included within
+                    # The given darknet model. Documentation for the original needs to be found and researched
+                    # TODO: Read documentation for original Darknet model
                     output = self.darknet_model(p_img_batch)
                     max_prob = self.prob_extractor(output)
                     nps = self.nps_calculator(adv_patch)
                     tv = self.total_variation(adv_patch)
 
-
+                    # Calculates the loss in the new patch, then mashes them all together
                     nps_loss = nps*0.01
                     tv_loss = tv*2.5
                     det_loss = torch.mean(max_prob)
@@ -112,15 +137,23 @@ class PatchTrainer(object):
                     ep_tv_loss += tv_loss.detach().cpu().numpy()
                     ep_loss += loss
 
+                    # Calculates the gradient of the loss function
                     loss.backward()
+
+                    # Performs one step in optimization of the patch
+                    # TODO: Doesn't the optimizer need to know the gradient to do this? how does it interact with loss?
                     optimizer.step()
+
+                    # Clears all gradients after each step. Default is to accumulate them, we don't want that
                     optimizer.zero_grad()
-                    adv_patch_cpu.data.clamp_(0,1)       #keep patch in image range
+                    adv_patch_cpu.data.clamp_(0,1)       # keep patch in image range
 
                     bt1 = time.time()
                     if i_batch%5 == 0:
                         iteration = self.epoch_length * epoch + i_batch
 
+                        # Reads all the loss measurements into the Patchtrainer somewhere
+                        # I assume this is how optimizer interacts with loss
                         self.writer.add_scalar('total_loss', loss.detach().cpu().numpy(), iteration)
                         self.writer.add_scalar('loss/det_loss', det_loss.detach().cpu().numpy(), iteration)
                         self.writer.add_scalar('loss/nps_loss', nps_loss.detach().cpu().numpy(), iteration)
@@ -128,23 +161,29 @@ class PatchTrainer(object):
                         self.writer.add_scalar('misc/epoch', epoch, iteration)
                         self.writer.add_scalar('misc/learning_rate', optimizer.param_groups[0]["lr"], iteration)
 
+                        # Saves the current patch into the trainer somewhere
+                        # TODO: Figure out exactly where this and the loss metrics are saved
                         self.writer.add_image('patch', adv_patch_cpu, iteration)
+
+                        # If the training is over, add an endline character, else clearn the following variables
                     if i_batch + 1 >= len(train_loader):
                         print('\n')
                     else:
                         del adv_batch_t, output, max_prob, det_loss, p_img_batch, nps_loss, tv_loss, loss
                         torch.cuda.empty_cache()
                     bt0 = time.time()
+
+            # Calculate average loss over the course of training
             et1 = time.time()
             ep_det_loss = ep_det_loss/len(train_loader)
             ep_nps_loss = ep_nps_loss/len(train_loader)
             ep_tv_loss = ep_tv_loss/len(train_loader)
             ep_loss = ep_loss/len(train_loader)
 
-            #im = transforms.ToPILImage('RGB')(adv_patch_cpu)
-            #plt.imshow(im)
-            #plt.savefig(f'pics/{time_str}_{self.config.patch_name}_{epoch}.png')
-
+            # im = transforms.ToPILImage('RGB')(adv_patch_cpu)
+            # plt.imshow(im)
+            # plt.savefig(f'pics/{time_str}_{self.config.patch_name}_{epoch}.png')
+            # Output statistics and training time
             scheduler.step(ep_loss)
             if True:
                 print('  EPOCH NR: ', epoch),
@@ -153,10 +192,10 @@ class PatchTrainer(object):
                 print('  NPS LOSS: ', ep_nps_loss)
                 print('   TV LOSS: ', ep_tv_loss)
                 print('EPOCH TIME: ', et1-et0)
-                #im = transforms.ToPILImage('RGB')(adv_patch_cpu)
-                #plt.imshow(im)
-                #plt.show()
-                #im.save("saved_patches/patchnew1.jpg")
+                # im = transforms.ToPILImage('RGB')(adv_patch_cpu)
+                # plt.imshow(im)
+                # plt.show()
+                # im.save("saved_patches/patchnew1.jpg")
                 del adv_batch_t, output, max_prob, det_loss, p_img_batch, nps_loss, tv_loss, loss
                 torch.cuda.empty_cache()
             et0 = time.time()
@@ -191,6 +230,7 @@ class PatchTrainer(object):
         return adv_patch_cpu
 
 
+# Checks for the correct input length and then runs the trainer
 def main():
     if len(sys.argv) != 2:
         print('You need to supply (only) a configuration mode.')
