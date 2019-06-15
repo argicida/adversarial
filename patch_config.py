0b142aa8646732b4cfef8bc11db2e8ce1ad10ed5
PerryXDeng <pxd256@protonmail.com>
2019-06-15 18:37:49 -0400

    now includes inria and will run until it runs out of GPU cache

--- patch_config.py
+++ patch_config.py
@@ -12,8 +12,8 @@ class BaseConfig(object):
         """
         self.img_dir = "inria/Train/pos"
         self.lab_dir = "inria/Train/pos/yolo-labels"
-        self.cfgfile = "cfg/yolo.cfg"
-        self.weightfile = "weights/yolo.weights"
+        self.cfgfile = "cfg/yolov2.cfg"
+        self.weightfile = "weights/yolov2.weights"
         self.printfile = "non_printability/30values.txt"
         self.patch_size = 300
 
