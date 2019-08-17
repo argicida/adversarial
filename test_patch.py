c1860b5b39cdc1ab3d638a28d4bd92264963f147
PerryXDeng <pxd256@protonmail.com>
2019-08-17 13:26:08 -0400

    fixed settings for patch testing

--- test_patch.py
+++ test_patch.py
@@ -17,10 +17,10 @@ import json
 if __name__ == '__main__':
     print("Setting everything up")
     imgdir = "inria/Test/pos"
-    cfgfile = "cfg/yolo.cfg"
-    weightfile = "weights/yolo.weights"
+    cfgfile = "cfg/yolov2.cfg"
+    weightfile = "weights/yolov2.weights"
     patchfile = "saved_patches/patch11.jpg"
-    patchfile = "/home/wvr/Pictures/individualImage_upper_body.png"
+    #patchfile = "/home/wvr/Pictures/individualImage_upper_body.png"
     #patchfile = "/home/wvr/Pictures/class_only.png"
     #patchfile = "/home/wvr/Pictures/class_transfer.png"
     savedir = "testing"
