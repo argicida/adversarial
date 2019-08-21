a4d7f180c40f140b2427d087e884f560aff220dd
nglod33 <nglod33@gmail.com>
2019-08-21 12:20:16 -0400

    Typos and documentation

--- analyze_json.py
+++ analyze_json.py
@@ -1,13 +1,13 @@
 # Author: Nate Glod
 # Filename: analyze_json.py
-# Script to intepret the json results for test_patch.py. Automatically tests all the
+# Script to intepret the json results for test_patch.py.
 import pandas as pd
 
 
 def interpret_results(filename):
     df = pd.read_json(filename)
     total_count = df.count().values[0]
-    df = df[df.score < 0.4]
+    df = df[df.score < 0.25]
     success_count = df.count().values[0]
     success_rate = success_count/total_count
     print("Success Rate for " + filename + ": " + str(success_rate))
@@ -17,7 +17,7 @@ def main():
     '''
     The 'score' category in the json measures object score, how likely the algorithm this there is an object where the
     person is in the picture. The threshold for saying there is an object where the person is supposed to be is
-    0.4, and this script prints the percentage of the time that each approach evaded detection
+    0.25, and this script prints the the percentage of times the given approach evaded detection successfully
     :return:
     '''
     # No patch, no noise, just a clean picture
