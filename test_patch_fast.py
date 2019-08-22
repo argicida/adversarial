84e9a30b132a8a787ca041c3a71762a29187bb81
nglod33 <nglod33@gmail.com>
2019-08-22 10:50:23 -0400

    Trying fast test_patch for statistical analysis

---
+++ test_patch_fast.py
@@ -0,0 +1,157 @@
+import sys
+import time
+import os
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from torchvision import transforms
+from PIL import Image, ImageDraw
+from utils import *
+from darknet import *
+from load_data import PatchTransformer, PatchApplier, InriaDataset
+import json
+
+
+if __name__ == '__main__':
+    print("Setting everything up")
+    imgdir = "inria/Test/pos"
+    cfgfile = "cfg/yolov2.cfg"
+    weightfile = "weights/yolov2.weights"
+    # To change the patch you're testing, change the patchfile variable to the path of the desired patch
+    patchfile = "saved_patches/patch11.jpg"
+    savedir = "testing"
+
+    darknet_model = Darknet(cfgfile)
+    darknet_model.load_weights(weightfile)
+    darknet_model = darknet_model.eval().cuda()
+    patch_applier = PatchApplier().cuda()
+    patch_transformer = PatchTransformer().cuda()
+
+    batch_size = 1
+    max_lab = 14
+    img_size = darknet_model.height
+
+    patch_size = 300
+
+    # Transform image to correct size
+    patch_img = Image.open(patchfile).convert('RGB')
+    tf = transforms.Resize((patch_size,patch_size))
+    patch_img = tf(patch_img)
+    # create tensor to represent image
+    tf = transforms.ToTensor()
+    adv_patch_cpu = tf(patch_img)
+    adv_patch = adv_patch_cpu.cuda()
+
+    clean_results = 0
+    noise_results = 0
+    patch_results = 0
+
+    # Walk over clean images
+    for imgfile in os.listdir(imgdir):
+        if imgfile.endswith('.jpg') or imgfile.endswith('.png'):
+            name = os.path.splitext(imgfile)[0]    # image name w/o extension
+            txtname = name + '.txt'
+            txtpath = os.path.abspath(os.path.join(savedir, 'clean/', 'yolo-labels/', txtname))
+            # open image and adjust to yolo input size
+            imgfile = os.path.abspath(os.path.join(imgdir, imgfile))
+            img = Image.open(imgfile).convert('RGB')
+            w,h = img.size
+            # ensure image is square
+            if w==h:
+                padded_img = img
+            else:
+                # pad image with grey
+                dim_to_pad = 1 if w<h else 2
+                if dim_to_pad == 1:
+                    padding = (h - w) / 2
+                    padded_img = Image.new('RGB', (h,h), color=(127,127,127))
+                    padded_img.paste(img, (int(padding), 0))
+                else:
+                    padding = (w - h) / 2
+                    padded_img = Image.new('RGB', (w, w), color=(127,127,127))
+                    padded_img.paste(img, (0, int(padding)))
+            # resize image to fit into yolo neural net
+            resize = transforms.Resize((img_size,img_size))
+            padded_img = resize(padded_img)
+            cleanname = name + ".png"
+            # save this image
+            padded_img.save(os.path.join(savedir, 'clean/', cleanname))
+
+            """ at this point, clean images are prepped to be analyzed by yolo """
+
+            # generate a label file for the padded image
+            boxes = do_detect(darknet_model, padded_img, 0.4, 0.4, True) # run yolo object detection on image
+            boxes = nms(boxes, 0.4) # run non-maximum suppression to remove redundant boxes
+            textfile = open(txtpath,'w+')
+            for box in boxes:
+                cls_id = box[6]
+                if(cls_id == 0):   # if person
+                    x_center = box[0]
+                    y_center = box[1]
+                    width = box[2]
+                    height = box[3]
+                    textfile.write(f'{cls_id} {x_center} {y_center} {width} {height}\n')
+                    # add detected box to label file (only for people)
+                    clean_results = clean_results + 1
+            textfile.close()
+
+            """ At this point, image recognition has been ran, and humans detected in images have been tracked"""
+
+            # read this label file back as a tensor
+            if os.path.getsize(txtpath):       #check to see if label file contains data. 
+                label = np.loadtxt(txtpath)
+            else:
+                label = np.ones([5])
+            label = torch.from_numpy(label).float()
+            if label.dim() == 1:
+                # Eric: Unsure what purpose this unsqueeze serves
+                label = label.unsqueeze(0)
+
+            # Tensorify the image
+            transform = transforms.ToTensor()
+            padded_img = transform(padded_img).cuda()
+            img_fake_batch = padded_img.unsqueeze(0)
+            lab_fake_batch = label.unsqueeze(0).cuda()
+            
+            # transform patch and add it to image
+            adv_batch_t = patch_transformer(adv_patch, lab_fake_batch, img_size, do_rotate=True, rand_loc=False)
+            p_img_batch = patch_applier(img_fake_batch, adv_batch_t)
+            p_img = p_img_batch.squeeze(0)
+            p_img_pil = transforms.ToPILImage('RGB')(p_img.cpu())
+            properpatchedname = name + "_p.png"
+            p_img_pil.save(os.path.join(savedir, 'proper_patched/', properpatchedname))
+            
+            # check to see if the generated patch fools recognition
+            boxes = do_detect(darknet_model, p_img_pil, 0.01, 0.4, True)
+            boxes = nms(boxes, 0.4)
+            for box in boxes:
+                cls_id = box[6]
+                if(cls_id == 0):   # if person
+                    if box[4].item() < 0.4: # if the threshold for detecting a person is met
+                        patch_results = patch_results + 1
+
+            # make a random patch, transform it and add it to the image
+            random_patch = torch.rand(adv_patch_cpu.size()).cuda()
+            adv_batch_t = patch_transformer(random_patch, lab_fake_batch, img_size, do_rotate=True, rand_loc=False)
+            p_img_batch = patch_applier(img_fake_batch, adv_batch_t)
+            p_img = p_img_batch.squeeze(0)
+            p_img_pil = transforms.ToPILImage('RGB')(p_img.cpu())
+            properpatchedname = name + "_rdp.png"
+            p_img_pil.save(os.path.join(savedir, 'random_patched/', properpatchedname))
+            
+            # check to see if the random patch evades recognition
+            boxes = do_detect(darknet_model, p_img_pil, 0.01, 0.4, True)
+            boxes = nms(boxes, 0.4)
+            for box in boxes:
+                cls_id = box[6]
+                if(cls_id == 0):   # if person
+                    if boxes[4].item() < 0.4: # If the
+                        noise_results = noise_results + 1
+
+print("clean_results: " + clean_results/clean_results)
+print("noise_results: " + noise_results/clean_results)
+print("patch_results: " + patch_results/clean_results)
+            
+
