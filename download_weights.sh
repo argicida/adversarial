5da9cc03476962c7d34d0ae663f243b9169bead3
Nate Glod (RIT Student) <nxg8159@sporcsubmit.rc.rit.edu>
2019-09-28 13:41:30 -0400

    Added to the get_weights script

--- download_weights.sh
+++ download_weights.sh
@@ -3,3 +3,4 @@
 # Downloads model weights (as they're too big to fit in the git repo)
 
 curl https://pjreddie.com/media/files/yolov2.weights -o weights/yolov2.weights
+curl https://s3.amazonaws.com/amdegroot-models/ssd300_mAP_77.43_v2.pth -o implementations/ssd_pytorch/weights
