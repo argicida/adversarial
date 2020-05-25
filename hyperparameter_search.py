6131e80e1950ca646aa3034aeb66e6f8f91fc3dd
Cooper W Linsky <cwl4602@sporcsubmit.rc.rit.edu>
2020-05-25 18:36:03 -0400

    Hyperparameter search now loaded with JSON

--- hyperparameter_search.py
+++ hyperparameter_search.py
@@ -1,5 +1,4 @@
 import os
-import argparse
 from datetime import datetime
 
 import ConfigSpace as CS
@@ -10,34 +9,20 @@ from ray.tune.suggest.bohb import TuneBOHB
 
 from train_test_patch_one_gpu import train
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--mbs", type=int, default=8, help="minibatch size")
-parser.add_argument("--ti", type=int, default=10, help="number of epochs in a tracking interval, "
-                                                       "0 to disable interval tracking")
-parser.add_argument("--ni", type=int, default=5, help="number of tracking intervals in a session")
-parser.add_argument("--ne", type=int, default=51, help="number of epochs in a session, "
-                                                       "for when interval tracking is disabled")
-
-args = parser.parse_args()
-mini_batch_size = args.mbs
-tracking_interval = args.ti
-n_epochs = tracking_interval*args.ni + 1 if args.ti != 0 else args.ne
-
+tracking_interval = 10
 standard_flags = f'--eval_yolov2=True --eval_ssd=True --eval_yolov3=True ' \
-                 f'--n_epochs={n_epochs} --mini_bs={mini_batch_size} ' \
                  f'--inria_train_dir=../../inria/Train/pos --printable_vals_filepath=../../non_printability/30values.txt ' \
                  f'--inria_test_dir=../../inria/Test/pos --logdir=logs --yolov2_cfg_file=../../cfg/yolov2.cfg ' \
                  f'--yolov2_weight_file=../../weights/yolov2.weights --yolov3_cfg_file=../../implementations/yolov3/config/yolov3.cfg ' \
                  f'--yolov3_weight_file=../../implementations/yolov3/weights/yolov3.weights ' \
                  f'--ssd_weight_file=../../implementations/ssd/models/vgg16-ssd-mp-0_7726.pth ' \
                  f'--example_patch_file=../../saved_patches/perry_08-26_500_epochs.jpg ' \
-                 f'--tensorboard_epoch=False'
-
+                 f'--tensorboard_epoch=False --train_ssd=1'
 
 def train_one_gpu(config):
     flags = f'python3 ../../train_test_patch_one_gpu.py {standard_flags}'
     for i in config:
-      flags+=f' --{i}={str(config[i])}'
+      flags+=f' --{i}={str(config[i])}' 
     os.system(flags)
     if os.path.exists("logs/metric.txt"):
       textfile = open("logs/metric.txt", 'r')
@@ -58,60 +43,41 @@ def train_one_gpu_early_stopping(config):
   argv = flags.split()[1:]
   FLAGS(argv)
   train()
-  if tracking_interval is 0:
-    if os.path.exists("logs/metric.txt"):
-      textfile = open("logs/metric.txt", 'r')
-      metric = float(textfile.readline())
-      textfile.close()
-      os.remove("logs/metric.txt")
-      tune.track.log(worst_case_iou=metric, done=True)
-    else:
-      raise Exception("trial failed")
+  # if os.path.exists("logs/metric.txt"):
+    # textfile = open("logs/metric.txt", 'r')
+    # metric = float(textfile.readline())
+    # textfile.close()
+    # os.remove("logs/metric.txt")
+    # tune.track.log(worst_case_iou=metric, done=True)
+  # else:
+    # print("Trial Failed!")
     
-    
+with open(config_file) as config_file:
+    data = json.load(config_file)
+
+n_samples = data['hyperparameter_search_settings']['num_samples']
+config = data['hyperparameter_config_space']
+mini_batch_size = config['constants']['mini_batch_size']
+tracking_interval = int(config['constants']['tracking_interval'])
+n_epochs = tracking_interval * 5
+standard_flags+=f' --n_epochs={n_epochs} --mini_bs={mini_batch_size}' 
+
 config_space = CS.ConfigurationSpace()
-config_space.add_hyperparameter(CS.UniformFloatHyperparameter("lr", lower=0.00001, upper=.1))
-config_space.add_hyperparameter(CS.UniformIntegerHyperparameter("num_mini", lower=1, upper=20))
-config_space.add_hyperparameter(CS.UniformIntegerHyperparameter("plateau_patience", lower=1, upper=n_epochs))
-config_space.add_hyperparameter(CS.CategoricalHyperparameter("activate_logits", choices=["True","False"]))
-config_space.add_hyperparameter(CS.CategoricalHyperparameter("confidence_processor", choices=["avg", "max", "det_max", "det_avg", "det_max_avg"]))
-
-min_max_hp = CS.CategoricalHyperparameter("minimax", choices=["True","False"])
-config_space.add_hyperparameter(min_max_hp)
-
-minimax_gamma_hp = CS.UniformFloatHyperparameter("minimax_gamma", lower=0.00001, upper=100)
-config_space.add_hyperparameter(minimax_gamma_hp)
-config_space.add_condition(CS.EqualsCondition(minimax_gamma_hp, min_max_hp, "True"))
-
-max_optim_hp = CS.CategoricalHyperparameter("max_optim", choices=["sgd","adam"])
-config_space.add_hyperparameter(max_optim_hp)
-config_space.add_condition(CS.EqualsCondition(max_optim_hp, min_max_hp, "True"))
-
-max_lr_hp = CS.UniformFloatHyperparameter("max_lr", lower=0.00001, upper=.1)
-config_space.add_hyperparameter(max_lr_hp)
-config_space.add_condition(CS.EqualsCondition(max_lr_hp, min_max_hp, "True"))
-
-
-config_space.add_hyperparameter(CS.CategoricalHyperparameter("start_patch", choices=["grey","random"]))
-
-train_yolov2_hp = CS.CategoricalHyperparameter("train_yolov2", choices=["1","2","3"])
-config_space.add_hyperparameter(train_yolov2_hp)
-
-train_yolov3_hp = CS.CategoricalHyperparameter("train_yolov3", choices=["1","2","3"])
-config_space.add_hyperparameter(train_yolov3_hp)
-
-config_space.add_hyperparameter(CS.CategoricalHyperparameter("train_ssd", choices=["1"]))
-config_space.add_hyperparameter(CS.UniformFloatHyperparameter("yolov2_prior_weight", lower=-10, upper=10))
-config_space.add_hyperparameter(CS.UniformFloatHyperparameter("ssd_prior_weight", lower=-10, upper=10))
-config_space.add_hyperparameter(CS.UniformFloatHyperparameter("yolov3_prior_weight", lower=-10, upper=10))
-
-yolov2_object_weight_hp = CS.UniformFloatHyperparameter("yolov2_object_weight", lower=0.00001, upper=1)
-config_space.add_hyperparameter(yolov2_object_weight_hp)
-config_space.add_condition(CS.EqualsCondition(yolov2_object_weight_hp, train_yolov2_hp, "3"))
-
-yolov3_object_weight_hp = CS.UniformFloatHyperparameter("yolov3_object_weight", lower=0.00001, upper=1)
-config_space.add_hyperparameter(yolov3_object_weight_hp)
-config_space.add_condition(CS.EqualsCondition(yolov3_object_weight_hp, train_yolov3_hp, "3"))
+constraints = {}
+for name,settings in config['search_space'].items():
+  hp_type = settings['type']
+  if hp_type == 'UF':
+    hp = CS.UniformFloatHyperparameter(name, lower=float(settings['lower']), upper=float(settings['upper']))
+  elif hp_type == 'UI':
+    hp = CS.UniformIntegerHyperparameter(name, lower=float(settings['lower']), upper=float(settings['upper']))
+  elif hp_type == 'C':
+    hp = CS.CategoricalHyperparameter(name, choices=settings['options'].split(','))
+  config_space.add_hyperparameter(hp)
+  if name == 'train_yolov2' or name == 'train_yolov3' or name == 'minimax':
+    constraints[name] = hp
+  if 'condition' in settings:
+    conditions = settings['condition'].split(',')
+    config_space.add_condition(CS.EqualsCondition(hp, constraints[conditions[0]], conditions[1]))
 
 experiment_metrics = dict(metric="worst_case_iou", mode="min")
 bohb_hyperband = HyperBandForBOHB(time_attr="training_iteration",max_t=n_epochs,**experiment_metrics)
@@ -122,5 +88,5 @@ analysis = tune.run(train_one_gpu_early_stopping,
     name=f"logs_hp_optim_{_init_time.astimezone().tzinfo.tzname(None)+_init_time.strftime('%Y%m%d_%H_%M_%S_%f')}",
     scheduler=bohb_hyperband,
     search_alg=bohb_search,
-    num_samples=10, resources_per_trial={"gpu":1}, local_dir="./")
+    num_samples=n_samples, resources_per_trial={"gpu":1}, local_dir="./")
 print("Best config: ", analysis.get_best_config(metric="worst_case_iou", mode="min"))
