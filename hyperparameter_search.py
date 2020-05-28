198b102604cb32fcd0fbe24da764ab4019ff0f51
Cooper W Linsky <cwl4602@sporcsubmit.rc.rit.edu>
2020-05-28 17:31:12 -0400

    Hyperparameters now loaded with JSON.

--- hyperparameter_search.py
+++ hyperparameter_search.py
@@ -1,4 +1,7 @@
 import os
+import sys
+import argparse
+import json
 from datetime import datetime
 
 import ConfigSpace as CS
@@ -9,7 +12,6 @@ from ray.tune.suggest.bohb import TuneBOHB
 
 from train_test_patch_one_gpu import train
 
-tracking_interval = 10
 standard_flags = f'--eval_yolov2=True --eval_ssd=True --eval_yolov3=True ' \
                  f'--inria_train_dir=../../inria/Train/pos --printable_vals_filepath=../../non_printability/30values.txt ' \
                  f'--inria_test_dir=../../inria/Test/pos --logdir=logs --yolov2_cfg_file=../../cfg/yolov2.cfg ' \
@@ -22,7 +24,7 @@ standard_flags = f'--eval_yolov2=True --eval_ssd=True --eval_yolov3=True ' \
 def train_one_gpu(config):
     flags = f'python3 ../../train_test_patch_one_gpu.py {standard_flags}'
     for i in config:
-      flags+=f' --{i}={str(config[i])}' 
+      flags+=f' --{i}={str(config[i])}'
     os.system(flags)
     if os.path.exists("logs/metric.txt"):
       textfile = open("logs/metric.txt", 'r')
@@ -37,31 +39,50 @@ def train_one_gpu(config):
 def train_one_gpu_early_stopping(config):
   from cli_config import FLAGS
   FLAGS.unparse_flags()
-  flags = f'python3 ../../train_test_patch_one_gpu.py {standard_flags} --tune_tracking_interval={tracking_interval}'
+  flags = f'python3 ../../train_test_patch_one_gpu.py {standard_flags}'
   for i in config:
     flags += f' --{i}={str(config[i])}'
   argv = flags.split()[1:]
   FLAGS(argv)
   train()
-  # if os.path.exists("logs/metric.txt"):
-    # textfile = open("logs/metric.txt", 'r')
-    # metric = float(textfile.readline())
-    # textfile.close()
-    # os.remove("logs/metric.txt")
-    # tune.track.log(worst_case_iou=metric, done=True)
-  # else:
-    # print("Trial Failed!")
+
+# Define logdir file, create it if does not exist
+_init_time = datetime.now()
+logdir = f"logs_hpo_{_init_time.astimezone().tzinfo.tzname(None)+_init_time.strftime('%Y%m%d_%H_%M_%S_%f')}"
+
+if not os.path.exists(logdir):
+  os.makedirs(logdir)
+if setting_list['redirect_stdout'] == 'True':
+  sys.stdout = open(os.path.join(logdir, "stdout.txt"), "w")
     
+# Load JSON config file
 with open(config_file) as config_file:
     data = json.load(config_file)
 
-n_samples = data['hyperparameter_search_settings']['num_samples']
+# Extract settings + hyperparameter config
 config = data['hyperparameter_config_space']
-mini_batch_size = config['constants']['mini_batch_size']
-tracking_interval = int(config['constants']['tracking_interval'])
-n_epochs = tracking_interval * 5
+setting_list = data['settings']
+
+# Get number of samples and mini batch size
+n_samples = setting_list['n_samples']
+mini_batch_size = setting_list['mini_batch_size']
+
+# Check if early stopping is enabled. If it is, include tracking interval in flags. Also calculate n_epochs
+if setting_list['early_stopping'] == 'True':
+  tracking_interval = setting_list['tracking_interval']
+  standard_flags+=f" --tune_tracking_interval={tracking_interval}"
+  n_epochs = int(tracking_interval)*int(setting_list['n_tracking_intervals'])
+else:
+  n_epochs = int(setting_list['n_epochs'])
+
+# Add n_epochs and mini batch size to flags
 standard_flags+=f' --n_epochs={n_epochs} --mini_bs={mini_batch_size}' 
 
+# Add constant hyperparameters to flags
+for constant,value in config['constants'].items():
+  standard_flags+=f' --{constant}={value}' 
+  
+# Extract hyperparameters from JSON file and add to configuration space. Also account for any constraints.
 config_space = CS.ConfigurationSpace()
 constraints = {}
 for name,settings in config['search_space'].items():
@@ -79,14 +100,19 @@ for name,settings in config['search_space'].items():
     conditions = settings['condition'].split(',')
     config_space.add_condition(CS.EqualsCondition(hp, constraints[conditions[0]], conditions[1]))
 
+# Run hyperparameter optimization
 experiment_metrics = dict(metric="worst_case_iou", mode="min")
 bohb_hyperband = HyperBandForBOHB(time_attr="training_iteration",max_t=n_epochs,**experiment_metrics)
 bohb_search = TuneBOHB(config_space, **experiment_metrics)
 
-_init_time = datetime.now()
 analysis = tune.run(train_one_gpu_early_stopping,
-    name=f"logs_hp_optim_{_init_time.astimezone().tzinfo.tzname(None)+_init_time.strftime('%Y%m%d_%H_%M_%S_%f')}",
+    name=logdir,
     scheduler=bohb_hyperband,
     search_alg=bohb_search,
     num_samples=n_samples, resources_per_trial={"gpu":1}, local_dir="./")
+    
 print("Best config: ", analysis.get_best_config(metric="worst_case_iou", mode="min"))
+
+# saves relevant summary data to file under logdir
+df = analysis.dataframe()
+df.to_csv(os.path.join(logdir, "data.csv"))
